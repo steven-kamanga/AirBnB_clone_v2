014300940294ddc7f0f9f71b440f95cd0533298e
steven-kamanga <kamangasteven00@gmail.com>
2023-08-16 20:03:38 +0000

    small changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """Handles the root url"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+    app.run("0.0.0.0", 5003)
